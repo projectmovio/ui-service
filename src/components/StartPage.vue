d351e7373bf951bf76ed3d2fd1bc37bf70e1da0c
Truong Hoang <truong@pingpong.net>
2017-10-27 17:00:32 +0200

    Infinite Scroll

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,26 +1,19 @@
 <template>
-  <v-layout row>
-    <v-flex xs10 offset-xs1>
-      <v-container mt-5 fluid grid-list-sm>
-        <v-layout row wrap justify-center>
-          <v-flex ml-2 mb-2 mt-2 mr-2 xs2 v-for="movie in movies" :key="movie.id">
-            <v-card>
-              <v-card-media :src="`http://image.tmdb.org/t/p/w185/${movie.backdrop_path}`" height="230">
-              </v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="truncate headline mb-0">{{movie.title}}</h3>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn color="primary">Add</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-flex>
-  </v-layout>
+    <v-layout v-scroll="onScroll">
+        <v-flex xs12 sm8 offset-sm2>
+            <v-container fluid v-bind="{ [`grid-list-xs`]: true }">
+                <v-layout row wrap>
+                    <transition-group name="list" tag="p">
+                        <span v-for="item in movies" v-bind:key="item.id" class="list-item">
+                            <img :src="`http://image.tmdb.org/t/p/w600/${item.poster_path}`" width="270px" height="400px" />
+                        </span>
+                    </transition-group>
+                </v-layout>
+            </v-container>
+
+            <v-btn @click.native="loadMore" color="primary">Load More</v-btn>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
@@ -29,7 +22,9 @@ import Vue from 'vue'
 export default {
   data() {
     return {
-      movies: []
+      movies: [],
+      currentPageLoaded: 1,
+      scrollOffsetBottom: 1000
     }
   },
   created() {
@@ -47,15 +42,51 @@ export default {
       })
   },
   methods: {
+    loadMore() {
+      Vue.axios
+        .get(
+          `http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=${this
+            .currentPageLoaded + 1}`,
+          {
+            params: {
+              api_key: '667cd61fb50d8c5fff3ac5f37fd760b7'
+            }
+          }
+        )
+        .then(response => {
+          this.movies = this.movies.concat(response.data.results)
+          this.currentPageLoaded++
+        })
+    },
+    onScroll(e) {
+      let pos =
+        (document.documentElement.scrollTop || document.body.scrollTop) +
+        document.documentElement.offsetHeight
+      let max = document.documentElement.scrollHeight
+      if (pos == max) {
+          this.loadMore()
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-.truncate {
-  width: 200px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+img {
+  object-fit: cover;
+}
+
+.list-item {
+  margin: 2px;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
