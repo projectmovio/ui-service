d2ff86ce757bbf1882535836b7c85c78f840da9e
fulder <misad90@gmail.com>
2020-07-25 10:44:22 +0200

    Load oauth_authorize script in navbar

--- includes/js/oauth_authorize.js
+++ includes/js/oauth_authorize.js
@@ -4,7 +4,9 @@ function base64URLEncode(str) {
         .replace(/\//g, '_')
         .replace(/=/g, '');
 }
-var codeVerifier = base64URLEncode(crypto.randomBytes(32));
 
-document.cookie = 'codeVerifier=' + $codeVerifier;
-window.location.replace('https://michal-test.auth.eu-west-1.amazoncognito.com/authorize?code_challenge=' + codeVerifier + '&client_id=1sf96sadgj6gk91tnkdathrpi0&response_type=code&scope=email+openid&redirect_uri=https://192.168.1.203/MySite/movio/callback.html&code_challenge_method=S256')
+function authorize() {
+    var codeVerifier = base64URLEncode(crypto.randomBytes(32));
+    document.cookie = 'codeVerifier=' + $codeVerifier;
+    window.location.replace('https://michal-test.auth.eu-west-1.amazoncognito.com/authorize?code_challenge=' + codeVerifier + '&client_id=1sf96sadgj6gk91tnkdathrpi0&response_type=code&scope=email+openid&redirect_uri=https://192.168.1.203/MySite/movio/callback.html&code_challenge_method=S256')
+}
