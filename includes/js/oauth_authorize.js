5509128ce7145b3eae9f80a23514e5154486f271
fulder <misad90@gmail.com>
2020-07-25 12:39:45 +0200

    Add pkce_state and code verifier, fix flow bugs

--- includes/js/oauth_authorize.js
+++ includes/js/oauth_authorize.js
@@ -1,22 +1,54 @@
+//See: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript/1349462
+// dec2hex :: Integer -> String
+// i.e. 0-255 -> '00'-'ff'
+function dec2hex (dec) {
+  return dec < 10
+    ? '0' + String(dec)
+    : dec.toString(16)
+}
+// generateId :: Integer -> String
+function generateRandomString () {
+  var arr = new Uint8Array(40 / 2)
+  window.crypto.getRandomValues(arr)
+  return Array.from(arr, dec2hex).join('')
+}
+// --------------------
+
+// Calculate SHA-256 hash from verifier data and base64 encode it
+function sha256(verifier) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(verifier);
+    return window.crypto.subtle.digest('SHA-256', data);
+}
+async function codeChallengeFromVerifier(verifier) {
+    hashed = await sha256(verifier);
+    return base64URLEncode(hashed);
+}
 function base64URLEncode(str) {
-    return str.toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 }
 
-function authorize() {
-    var codeVerifier = base64URLEncode(crypto.randomBytes(32));
-    document.cookie = 'codeVerifier=' + $codeVerifier;
+async function authorize() {
+    codeVerifier = generateRandomString()
+
+    codeChallenge = await codeChallengeFromVerifier(codeVerifier)
+    state = generateRandomString()
+
+    console.log(codeChallenge)
+
+    localStorage.setItem("pkce_code_verifier", codeVerifier);
+    localStorage.setItem("pkce_state", state);
     
     const authorizeUrl = new URL("https://michal-test.auth.eu-west-1.amazoncognito.com/authorize");
 
-    authorizeUrl.searchParams.append("code_challange", codeVerifier);
+    authorizeUrl.searchParams.append("code_challenge", codeChallenge);
     authorizeUrl.searchParams.append("client_id", "1sf96sadgj6gk91tnkdathrpi0");
     authorizeUrl.searchParams.append("response_type", "code");
-    authorizeUrl.searchParams.append("scope", "email+openid");
+    authorizeUrl.searchParams.append("scope", "email openid");
     authorizeUrl.searchParams.append("redirect_uri", "https://192.168.1.203/MySite/movio/callback.html");
     authorizeUrl.searchParams.append("code_challenge_method", "S256");
+    authorizeUrl.searchParams.append("state", state);
 
     window.location.replace(authorizeUrl.href)
 }
--- includes/js/oauth_callback.js
+++ includes/js/oauth_callback.js
@@ -1,24 +1,36 @@
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
+const state = urlParams.get('state');
 
-const codeVerifier = document.cookie
-  .split('; ')
-  .find(row => row.startsWith('codeVerifier'))
-  .split('=')[1];
-document.cookie = 'codeVerifier=' + $codeVerifier;
+if(code === null) {
+    document.getElementById("login_status").innerHTML = "Login <b style='color:red'>failed</b>, forwarding back to home page"
+}
+else if (state != localStorage.getItem("pkce_state")) {
+    document.getElementById("login_status").innerHTML = "Login <b style='color:red'>failed</b>, forwarding back to home page"
+}
+else {
+    document.getElementById("login_status").innerHTML = "Login successful, forwarding back to home page"
+    const codeVerifier = localStorage.getItem("pkce_code_verifier");
 
-request = $.ajax({
-    url: "https://michal-test.auth.eu-west-1.amazoncognito.com/oauth2/token",
-    type: "post",
-    data: {
-        grant_type: "authorization_code",
-        redirect_uri: "https://192.168.1.203/MySite/movio/callback.html",
-        code: code,
-        client_id: "21sf96sadgj6gk91tnkdathrpi0",
-        code_verifier: codeVerifier
-    },
-    success:function(response) {
-        console.log(response)
-    },
-});
+    request = $.ajax({
+        url: "https://michal-test.auth.eu-west-1.amazoncognito.com/oauth2/token",
+        type: "post",
+        data: {
+            grant_type: "authorization_code",
+            redirect_uri: "https://192.168.1.203/MySite/movio/callback.html",
+            code: code,
+            client_id: "1sf96sadgj6gk91tnkdathrpi0",
+            code_verifier: codeVerifier
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        success:function(response) {
+            console.log(response)
+        },
+    });
+}
+
+localStorage.removeItem("pkce_code_verifier");
+localStorage.removeItem("pkce_state");
 
