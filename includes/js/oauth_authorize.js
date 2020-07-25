972688ce391e20970c01990dbdccaf1c099fcfb9
fulder <misad90@gmail.com>
2020-07-25 10:47:04 +0200

    Use URL with append

--- includes/js/oauth_authorize.js
+++ includes/js/oauth_authorize.js
@@ -8,5 +8,15 @@ function base64URLEncode(str) {
 function authorize() {
     var codeVerifier = base64URLEncode(crypto.randomBytes(32));
     document.cookie = 'codeVerifier=' + $codeVerifier;
-    window.location.replace('https://michal-test.auth.eu-west-1.amazoncognito.com/authorize?code_challenge=' + codeVerifier + '&client_id=1sf96sadgj6gk91tnkdathrpi0&response_type=code&scope=email+openid&redirect_uri=https://192.168.1.203/MySite/movio/callback.html&code_challenge_method=S256')
+    
+    const authorizeUrl = new URL("https://michal-test.auth.eu-west-1.amazoncognito.com/authorize");
+
+    authorizeUrl.searchParams.append("code_challange", codeVerifier);
+    authorizeUrl.searchParams.append("client_id", "1sf96sadgj6gk91tnkdathrpi0");
+    authorizeUrl.searchParams.append("response_type", "code");
+    authorizeUrl.searchParams.append("scope", "email+openid");
+    authorizeUrl.searchParams.append("redirect_uri", "https://192.168.1.203/MySite/movio/callback.html");
+    authorizeUrl.searchParams.append("code_challenge_method", "S256");
+
+    window.location.replace( authorizeUrl.href))
 }
