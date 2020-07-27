f5285d914a3b6c47dc2dc8d16896a39041b81aec
fulder <misad90@gmail.com>
2020-07-27 14:28:12 +0200

    Add search field with results page action

---
+++ includes/js/results.js
@@ -0,0 +1,17 @@
+const urlParams = new URLSearchParams(window.location.search);
+const searchString = urlParams.get('search');
+accessToken = localStorage.getItem("moshan_access_token")
+
+$.ajax({
+    url: "https://api.anime.moshan.tv/anime?search=" + searchString,
+    type: "get",
+    headers: {
+        'Authorization': accessToken
+    },
+    success:function(response) {
+        const animeResults = document.getElementById("animeResults")
+        animeResults.innerText = response
+    },
+});
+
+
