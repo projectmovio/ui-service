80de2772ce5744d2e4a8229ba1ff11f780afc0a4
fulder <misad90@gmail.com>
2020-07-27 12:36:52 +0200

    Use normal forward no ajax GET breaking CORS

--- includes/js/navbar.js
+++ includes/js/navbar.js
@@ -21,16 +21,9 @@ else {
 }
 
 function logout() {
-    $.ajax({
-        url: "https://auth.moshan.tv/logout",
-        type: "get",
-        data: {
-            logout_uri: "https://moshan.tv/index.html",
-            client_id: "68v5rahd0sdvrmf7fgbq2o1a9u",
-        }
-    });
     localStorage.removeItem("moshan_access_token");
     localStorage.removeItem("moshan_refresh_token");
+    window.location.replace("https://auth.moshan.tv/logout?logout_uri=https://moshan.tv/index.html&client_id=68v5rahd0sdvrmf7fgbq2o1a9u");
 }
 
 
