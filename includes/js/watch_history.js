711a86e3a9fee916cc04cc214059dc2854487be3
fulder <misad90@gmail.com>
2020-07-29 18:36:55 +0200

    Forgotten collection in path

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -12,3 +12,17 @@ function getWatchHistory() {
         },
     });
 }
+
+function getWatchHistoryByCollection(collection) {
+    $.ajax({
+        url: "https://api.watch-history.moshan.tv/v1/watch-history/collection/" + collection,
+        type: "get",
+        headers: {
+            'Authorization': accessToken
+        },
+        success:function(response) {
+            response = JSON.parse(response);
+            callback(response)
+        },
+    });
+}
