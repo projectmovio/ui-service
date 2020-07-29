6792622b2947a8816ae91fd37d1465cd9af34fce
fulder <misad90@gmail.com>
2020-07-29 12:11:03 +0200

    Forgotten commit items.js including addAnime function

---
+++ includes/js/items.js
@@ -0,0 +1,19 @@
+include("./common/token.js");
+
+function addAnime(animeId) {
+    $.ajax({
+        url: "https://api.watch-history.moshan.tv/v1/watch-history/collection/anime",
+        type: "post",
+        contentType: 'application/json',
+        dataType: "json",
+        data: JSON.stringify({
+            item_add_id: animeId
+        }),
+        headers: {
+            'Authorization': accessToken
+        },
+        error:function(response) {
+            console.log(response);
+        },
+    });
+}
