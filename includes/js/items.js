bbe20ffe85216201c1ff33abe09367de5e6a195d
fulder <misad90@gmail.com>
2020-07-29 15:13:57 +0200

    Add remove anime button if item in watchHistory

--- includes/js/items.js
+++ includes/js/items.js
@@ -1,11 +1,11 @@
-function addAnime(animeId) {
+function addAnime(malId) {
     $.ajax({
         url: "https://api.watch-history.moshan.tv/v1/watch-history/collection/anime",
         type: "post",
         contentType: 'application/json',
         dataType: "json",
         data: JSON.stringify({
-            item_add_id: animeId
+            item_add_id: malId
         }),
         headers: {
             'Authorization': accessToken
--- includes/js/results.js
+++ includes/js/results.js
@@ -11,6 +11,7 @@ $.ajax({
         animes = JSON.parse(response);
 
         resultHTML = ""
+        idMap = animes["id_map"]
         animes["items"].forEach(createResultItem);
 
         document.getElementById("animeResults").innerHTML = resultHTML
@@ -25,6 +26,14 @@ function createResultItem(anime) {
 
     resultHTML += '<div class="col-4 col-md-1 poster mx-md-1 px-md-1">'
     resultHTML +='<img class="img-fluid" src=' + poster + '>'
-    resultHTML +='<button class="btn btn-sm btn-success" onclick="addAnime(' + animeId + ')"><i class="fas fa-plus fa-xs"></i></button>'
+
+    // If the item is found in the idMap (i.e. the item is cache'd in moshan anime db)
+    // and if it is added to the user watch history, set the button to removeAnime button
+    // otherwise create the addAnime button
+    if (idMap[animeId] !== undefined && watchHistory["items"].find(item => item["collection_name"] == "anime" && item["item_id"] == idMap[animeId]) !== undefined) {
+        resultHTML +='<button class="btn btn-sm btn-danger" onclick="removeAnime(' + idMap[animeId] + ')"><i class="fas fa-minus fa-xs"></i></button>'
+    } else {
+        resultHTML +='<button class="btn btn-sm btn-success" onclick="addAnime(' + animeId + ')"><i class="fas fa-plus fa-xs"></i></button>'
+    }
     resultHTML += '<p class="text-truncate small">' + title + '</p></img></div>'
 }
