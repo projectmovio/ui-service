5205eb03fc016045231df843fe532e53f0d483e4
fulder <misad90@gmail.com>
2020-07-31 10:41:55 +0200

    Add loginInAlert to watch_history page

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -1,4 +1,10 @@
-document.getElementById("animeWatchHistory").innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>'
+if (accessToken === null) {
+    document.getElementById("logInAlert").className = "alert alert-danger";
+}
+else {
+    document.getElementById("logInAlert").className = "d-none";
+    document.getElementById("animeWatchHistory").innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>'
+}
 
 getWatchHistoryByCollection("anime", createAnimeItems);
 
