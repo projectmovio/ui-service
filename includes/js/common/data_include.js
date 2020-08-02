87830f2e0221c2cbe0c2167cf8ab3ef47053eac8
fulder <misad90@gmail.com>
2020-08-02 23:32:16 +0200

    Test including data_include

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -1,8 +1,17 @@
 //See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
 $(function(){
+    var path = document.location.pathname;
+
+    // https://stackoverflow.com/a/2161748
+    var scripts= document.getElementsByTagName('script');
+    var path= scripts[scripts.length-1].src
+    var scriptDir= path.split('/').slice(0, -1).join('/')+'/';
+
+    console.log(scriptDir);
+
     var includes = $('[data-include]');
     jQuery.each(includes, function(){
-      var file = 'includes/html/' + $(this).data('include') + '.html';
+      var file = directory + 'includes/html/' + $(this).data('include') + '.html';
       $(this).load(file);
     });
   });
