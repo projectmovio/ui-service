c103515cb082d0f41f7bf15828326656794c7e8f
fulder <misad90@gmail.com>
2020-08-02 19:47:31 +0200

    Add dataInclude function

---
+++ includes/js/common/data_include.js
@@ -0,0 +1,9 @@
+//See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
+$(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+      var file = 'includes/html/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+  });
+}
