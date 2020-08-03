05c6964f10812af5286debfa1d2fd58414353206
fulder <misad90@gmail.com>
2020-08-03 14:45:23 +0200

    Merge oauth_authorize into navbar, use jquery load again

---
+++ includes/js/common/data_include.js
@@ -0,0 +1,10 @@
+//See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
+var includes = document.querySelectorAll('[data-include]');
+
+includes.forEach(async function(includeDiv){
+  console.log(includeDiv);
+  var file = '/includes/html/' + includeDiv.getAttribute("data-include") + '.html';
+  response = await axios.get(file);
+  $(includeDiv).load(response.data);
+});
+
