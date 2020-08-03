95e1c49b57a8cc7f2174350f2e0f61acfc428049
fulder <misad90@gmail.com>
2020-08-04 00:29:52 +0200

    Use mal_id in getAnimeByMalId

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 
 mal_id = urlParams.get("mal_id")
 if (mal_id !== null) {
-    getAnimeByMalId(urlParams["mal_id"], createAnime)
+    getAnimeByMalId(mal_id, createAnime)
 }
 
 function createAnime(anime) {
