8052798ae584cc5f7234de804c2c1f9a59f0d9df
fulder <misad90@gmail.com>
2020-08-03 11:59:15 +0200

    Test loading navbar js from nabar.html

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -1,12 +1,11 @@
 //See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
-window.onload = function(){
-    var path = window.location.pathname;
+var path = window.location.pathname;
 
-    goBackFolders = window.location.pathname.split("/").length-2;
+goBackFolders = window.location.pathname.split("/").length-2;
 
-    var includes = $('[data-include]');
-    jQuery.each(includes, function(){
-      var file = "../".repeat(goBackFolders) + 'includes/html/' + $(this).data('include') + '.html';
-      $(this).load(file);
-    });
-};
+var includes = $('[data-include]');
+jQuery.each(includes, function(){
+  var file = "../".repeat(goBackFolders) + 'includes/html/' + $(this).data('include') + '.html';
+  $(this).load(file);
+});
+
