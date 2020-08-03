d6afd7d5711b732d2b34405bc7470ec448642d8c
fulder <misad90@gmail.com>
2020-08-03 11:32:35 +0200

    Test running data_include onload

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -1,5 +1,5 @@
 //See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
-$(function(){
+window.onload = function(){
     var path = window.location.pathname;
 
     goBackFolders = window.location.pathname.split("/").length-2;
@@ -9,4 +9,4 @@ $(function(){
       var file = "../".repeat(goBackFolders) + 'includes/html/' + $(this).data('include') + '.html';
       $(this).load(file);
     });
-});
+};
