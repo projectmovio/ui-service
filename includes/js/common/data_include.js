deffcb7a9107617a51f796cafbe26f61bf102fa8
fulder <misad90@gmail.com>
2020-08-03 12:27:01 +0200

    Test use Axios with vanilla JS in data_include

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -1,7 +1,11 @@
 //See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
-var includes = $('[data-include]');
-jQuery.each(includes, function(){
-  var file = '/includes/html/' + $(this).data('include') + '.html';
-  $(this).load(file);
+const axios = require('axios');
+
+var includes = document.querySelectorAll('[data-include]');
+
+includes.forEach(async function(includeDiv){
+  var file = '/includes/html/' + includeDiv["data-include"] + '.html';
+  fileContent = await axios.get(file);
+  includeDiv.innerHtml = fileContent;
 });
 
