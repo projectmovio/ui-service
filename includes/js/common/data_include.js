4d68bfbea92b6781b8117d190094876afa820d8c
fulder <misad90@gmail.com>
2020-08-03 13:37:22 +0200

    Debug log axios response

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -5,6 +5,7 @@ includes.forEach(async function(includeDiv){
   console.log(includeDiv);
   var file = '/includes/html/' + includeDiv.getAttribute("data-include") + '.html';
   fileContent = await axios.get(file);
+  console.log(fileContent);
   includeDiv.innerHtml = fileContent;
 });
 
