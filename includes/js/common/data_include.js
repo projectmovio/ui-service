1f3a2cf532502b091e77f337bbf65fa97eac625f
fulder <misad90@gmail.com>
2020-08-03 12:11:43 +0200

    Make all includes links relative root

--- includes/js/common/data_include.js
+++ includes/js/common/data_include.js
@@ -1,11 +1,7 @@
 //See: https://stackoverflow.com/a/31837264. Automated loading of html using "data-include" html tag
-var path = window.location.pathname;
-
-goBackFolders = window.location.pathname.split("/").length-2;
-
 var includes = $('[data-include]');
 jQuery.each(includes, function(){
-  var file = "../".repeat(goBackFolders) + 'includes/html/' + $(this).data('include') + '.html';
+  var file = '/includes/html/' + $(this).data('include') + '.html';
   $(this).load(file);
 });
 
