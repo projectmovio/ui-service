021c4acc47c79919764cfa2d98d6de4ed4079674
fulder <misad90@gmail.com>
2020-08-04 12:50:14 +0200

    Add link to anime page from watch_history items

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -34,9 +34,11 @@ function createHistoryAnimeItem(animeId, anime) {
     title = anime["title"];
     poster = anime["main_picture"]["medium"];
 
-    resultHTML += `<div id="poster-anime-${animeId}" class="col-4 col-md-2 poster">`
-    resultHTML += `<img class="img-fluid" src="${poster}">`
-    resultHTML += '<p class="text-truncate small">' + title + '</p></img></div>'
+    resultHTML += `<div id="poster-anime-${animeId}" class="col-4 col-md-2 poster">`;
+    resultHTML += `<a href="/anime/index.html?mal_id=${externalId}">`;
+    resultHTML += `<img class="img-fluid" src="${poster}">`;
+    resultHTML += '<p class="text-truncate small">' + title + '</p></img></div>';
+    resultHTML += '</a>';
 
     return true;
 }
