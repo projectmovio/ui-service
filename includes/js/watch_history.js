08cda17c104115f36cb72b6b6b7f4a0536ffaf42
fulder <misad90@gmail.com>
2020-08-04 23:08:49 +0200

    Use animeId from function params

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -33,10 +33,9 @@ function createHistoryAnimeItem(animeId, anime) {
 
     title = anime["title"];
     poster = anime["main_picture"]["medium"];
-    id = anime["id"];
 
     resultHTML += `<div id="poster-anime-${animeId}" class="col-4 col-md-2 poster">`;
-    resultHTML += `<a href="/anime/${id}">`;
+    resultHTML += `<a href="/anime/${animeId}">`;
     resultHTML += `<img class="img-fluid" src="${poster}">`;
     resultHTML += '<p class="text-truncate small">' + title + '</p></img></div>';
     resultHTML += '</a>';
