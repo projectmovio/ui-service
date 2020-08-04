c0bae673cb0c00ad96ba47e3dc52eb2e7e93d2ca
fulder <misad90@gmail.com>
2020-08-04 13:13:21 +0200

    Test move out title to header outside of grid

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -9,9 +9,10 @@ function createAnime(anime) {
     poster = anime["main_picture"]["large"]
     title = anime["title"]
 
+    document.getElementById("title").innerHTML = title
+
     resultHTML = '<div class="col-md-3 col-4 item">';
     resultHTML +='<img class="img-fluid" src=' + poster + '>';
-    resultHTML += '<p class="text-truncate">' + title + '</p></img></div>';
     document.getElementById("anime").innerHTML = resultHTML
 
     console.log(anime);
