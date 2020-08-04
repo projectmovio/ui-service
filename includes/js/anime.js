0f0356cb7ea5d66124bd67c9dd8bf670093c49e9
fulder <misad90@gmail.com>
2020-08-04 12:52:30 +0200

    Change anime div innerHTML in callback func

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -2,18 +2,17 @@ const urlParams = new URLSearchParams(window.location.search);
 
 mal_id = urlParams.get("mal_id")
 if (mal_id !== null) {
-    resultHTML = ""
     getAnimeByMalId(mal_id, createAnime)
-    document.getElementById("anime").innerHTML = resultHTML
 }
 
 function createAnime(anime) {
     poster = anime["main_picture"]["large"]
     title = anime["title"]
 
-    resultHTML += '<div class="col-2 item">';
+    resultHTML = '<div class="col-2 item">';
     resultHTML +='<img class="img-fluid" src=' + poster + '>';
     resultHTML += '<p class="text-truncate">' + title + '</p></img></div>';
+    document.getElementById("anime").innerHTML = resultHTML
 
     console.log(anime);
 }
