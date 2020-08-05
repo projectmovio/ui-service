6d595ca011bcd9dd27b3b21fd0a85feed38d0ea5
fulder <misad90@gmail.com>
2020-08-05 14:50:52 +0200

    Start adding collapse CSS to synopsis

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -22,18 +22,20 @@ function createAnime(anime) {
 
     document.getElementById("title").innerHTML = anime['title']
 
-    resultHTML = '<div class="col-md-3 col-4 item">';
+    resultHTML = '<div class="col-md-3 col-5 item">';
     resultHTML += `<img class="img-fluid" src="${poster}" />`;
     resultHTML += '</div>';
+    resultHTML += '<div class="col-md-9 col-12">';
+    resultHTML += `<p id="synopsis">${anime['synopsis']}</p>`;
+    resultHTML += `<a class="collapsed" data-toggle="collapse" href="#synopsis" aria-expanded="false" aria-controls="collapse"></a>`
+    resultHTML += '</div>';
 
-    resultHTML += '<div class="col-md-9 col-8">';
-    resultHTML += `<p><b>Release date</b>: ${anime['start_date']}</p>`;
+    resultHTML += '<div class="col-md-2 col-6">';
+    resultHTML += `<p><b>Date</b>: ${anime['start_date']}</p>`;
     resultHTML += `<p><b>Status</b>: ${status}</p>`;
     resultHTML += '</div>';
 
-    resultHTML += '<div class="col-md-9 col-12">';
-    resultHTML += `<p>${anime['synopsis']}</p>`;
-    resultHTML += '</div>';
+
     document.getElementById("anime").innerHTML = resultHTML
 
     console.log(anime);
