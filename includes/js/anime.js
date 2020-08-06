f565e88602f827cef6a464c91207d92a4fbabac2
fulder <misad90@gmail.com>
2020-08-06 12:26:13 +0200

    Forgotten '+'

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -39,7 +39,7 @@ function createAnime(anime) {
     resultHTML += '</div>';
 
     if ("anidb_id" in anime) {
-        resultHTML = '<div class="col-2">';
+        resultHTML += '<div class="col-2">';
         resultHTML += `<a href="https://anidb.net/anime/${anime['anidb_id']}"><img class="img-fluid external-link" src="/includes/icons/mal.png" /></a>`;
         resultHTML += '</div>';
     }
