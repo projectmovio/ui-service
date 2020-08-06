66935f98c3e828251f4b119d563c103375492d4a
fulder <misad90@gmail.com>
2020-08-06 11:56:50 +0200

    Add quotes around anime uuid

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -31,7 +31,7 @@ function createAnime(anime) {
     resultHTML += `<p><b>Status</b>: ${status}</p>`;
 
     resultHTML += `<button id="addButton" class="btn btn-success" onclick="addItem('anime', ${anime['mal_id']}, itemAdded)">Add</button>`;
-    resultHTML += `<button id="removeButton" class="btn btn-danger d-none" onclick="removeItem('anime', ${anime['id']}, itemRemoved)">Remove</button>`;
+    resultHTML += `<button id="removeButton" class="btn btn-danger d-none" onclick="removeItem('anime', '${anime['id']}', itemRemoved)">Remove</button>`;
     resultHTML += '</div>';
 
     resultHTML += '<div id="synopsisCol" class="mt-2 col-12">';
