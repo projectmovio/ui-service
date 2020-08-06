3f45026b5fee65eac49f240c88579af9ba95ae9e
fulder <misad90@gmail.com>
2020-08-06 11:12:23 +0200

    Debug returned anime on anime page

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -12,6 +12,7 @@ else if (mal_id !== null) {
 
 function createAnime(anime) {
     poster = anime["main_picture"]["large"]
+    console.log(anime);
 
     status = "Airing"
     if ("end_date" in anime) {
@@ -29,8 +30,8 @@ function createAnime(anime) {
     resultHTML += `<p><b>Released</b>: ${anime['start_date']}</p>`;
     resultHTML += `<p><b>Status</b>: ${status}</p>`;
 
-    resultHTML += `<button id="addButton" class="btn btn-success" onclick="addItem('anime', anime['mal_id'])">Add</button>`;
-    resultHTML += `<button id="removeButton" class="btn btn-danger d-none" onclick="removeItem('anime', anime['mal_id'])">Remove</button>`;
+    resultHTML += `<button id="addButton" class="btn btn-success" onclick="addItem('anime', anime['id'])">Add</button>`;
+    resultHTML += `<button id="removeButton" class="btn btn-danger d-none" onclick="removeItem('anime', anime['id'])">Remove</button>`;
     resultHTML += '</div>';
 
     resultHTML += '<div id="synopsisCol" class="mt-2 col-12">';
