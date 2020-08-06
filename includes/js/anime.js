ac385ac5175321288149c3d1c0b495a8fd3fd18b
fulder <misad90@gmail.com>
2020-08-06 12:44:28 +0200

    Tweak card sizes

--- includes/js/anime.js
+++ includes/js/anime.js
@@ -33,15 +33,15 @@ function createAnime(anime) {
     resultHTML += `<button id="addButton" class="btn btn-success" onclick="addItem('anime', ${anime['mal_id']}, itemAdded)">Add</button>`;
     resultHTML += `<button id="removeButton" class="btn btn-danger d-none" onclick="removeItem('anime', '${anime['id']}', itemRemoved)">Remove</button>`;
 
-    resultHTML += '<div class="card mt-1">'
-    resultHTML += '<div class="card-header">External Links</div>'
+    resultHTML += '<div class="card mt-2">'
+    resultHTML += '<div class="card-header small">External Links</div>'
     resultHTML += '<div class="card-body p-1">'
     resultHTML += '<div class="row">';
-    resultHTML += '<div class="col-5">';
+    resultHTML += '<div class="col-4">';
     resultHTML += `<a href="https://myanimelist.net/anime/${anime['mal_id']}" target="_blank"><img class="img-fluid" src="/includes/icons/mal.png" /></a>`;
     resultHTML += '</div>';
     if ("anidb_id" in anime) {
-        resultHTML += '<div class="col-5">';
+        resultHTML += '<div class="col-4">';
         resultHTML += `<a href="https://anidb.net/anime/${anime['anidb_id']}" target="_blank"><img class="img-fluid" src="/includes/icons/anidb.png" /></a>`;
         resultHTML += '</div>';
     }
