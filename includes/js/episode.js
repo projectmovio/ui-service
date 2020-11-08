6b06437e71323fcaad231c91cc798e095bd8ee7f
fulder <misad90@gmail.com>
2020-11-08 20:51:56 +0100

    Add buttons and watch date under poster for episodes

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -36,6 +36,9 @@ function createEpisodePage(animeEpisode, watchHistoryEpisode) {
             <h5>${animeEpisode['title']}</h5>
             <p><b>Aired</b>: ${animeEpisode['air_date']}</p>
             <p><b>Status</b>: ${status}</p>
+        </div>
+
+        <div class="col-md-3 col-7">
             <button id="addButton" class="btn btn-success ${!episodeAired || episodeAdded ? 'd-none' : ''}" onclick="addWatchHistoryEpisode('anime', ${animeEpisode['id']}, episodeAdded)"><i class="fa fa-plus"></i> Add</button>
             <button id="removeButton" class="btn btn-danger ${!episodeAired || !episodeAdded ? 'd-none' : ''}" onclick="removeWatchHistoryEpisode('anime', '${animeEpisode['id']}', episodeRemoved)"><i class="fa fa-minus"></i> Remove</button>
             <button class="btn btn-secondary ${!episodeAired ? '" disabled' : 'd-none"'}><i class="fa fa-plus"></i> Add</button>
