6249323e6401fa0598207de5e32bcda36489af68
fulder <misad90@gmail.com>
2020-11-14 11:16:48 +0100

    Comment away episode synopsis

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -50,16 +50,16 @@ function createEpisodePage(animeEpisode, watchHistoryEpisode) {
             </div>
         </div>
 
-        <div id="synopsisCol" class="mt-2 col-12">
-            <div class="card">
-                <a data-toggle="collapse" data-target="#collapseSynopsis" aria-expanded="true" aria-controls="collapseSynopsis">
-                    <div id="synopsisCardHeader" class="card-header">Synopsis</div>
-                </a>
-                <div id="collapseSynopsis" class="collapse" aria-labelledby="synopsisHeader" data-parent="#synopsisCol">
-                    <div class="card-body">${animeEpisode['synopsis']}</div>
-                </div>
-            </div>
-       </div>
+//        <div id="synopsisCol" class="mt-2 col-12">
+//            <div class="card">
+//                <a data-toggle="collapse" data-target="#collapseSynopsis" aria-expanded="true" aria-controls="collapseSynopsis">
+//                    <div id="synopsisCardHeader" class="card-header">Synopsis</div>
+//                </a>
+//                <div id="collapseSynopsis" class="collapse" aria-labelledby="synopsisHeader" data-parent="#synopsisCol">
+//                    <div class="card-body">${animeEpisode['synopsis']}</div>
+//                </div>
+//            </div>
+//       </div>
     `;
 
     document.getElementById("episode").innerHTML = resultHTML
