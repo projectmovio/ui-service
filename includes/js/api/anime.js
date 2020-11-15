ebbd236618292e7c75f2efb56db13577c58f3364
fulder <misad90@gmail.com>
2020-11-15 13:39:54 +0100

    Convert anime into AnimeApi class

--- includes/js/api/anime.js
+++ includes/js/api/anime.js
@@ -1,31 +1,35 @@
-const animeAxios = axios.create({
-  baseURL: 'https://api.anime.moshan.tv/v1',
-  headers: {
-    'Content-Type': 'application/json'
+class AnimeApi {
+  constructor () {
+    this.apiAxios = axios.create({
+      baseURL: 'https://api.anime.moshan.tv/v1',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    this.apiAxios.interceptors.request.use(axiosTokenInterceptor, function (error) {
+      console.log(error);
+      return Promise.reject(error);
+    });
   }
-});
 
-animeAxios.interceptors.request.use(axiosTokenInterceptor, function (error) {
-  console.log(error);
-  return Promise.reject(error);
-});
+  searchAnime (searchString) {
+    return .get(`/anime?search=${searchString}`);
+  }
 
-function searchAnime (searchString) {
-  return animeAxios.get(`/anime?search=${searchString}`);
-}
+  getAnimeByApiId (apiName, id) {
+    return this.apiAxios.get(`/anime?${apiName}_id=${id}`);
+  }
 
-function getAnimeByApiId (apiName, id) {
-  return animeAxios.get(`/anime?${apiName}_id=${id}`);
-}
+  getAnimeById (id) {
+    return this.apiAxios.get(`/anime/${id}`);
+  }
 
-function getAnimeById (id) {
-  return animeAxios.get(`/anime/${id}`);
-}
+  getAnimeEpisodes (id, start = 1, limit = 100) {
+    return this.apiAxios.get(`/anime/${id}/episodes?limit=${limit}&start=${start}`);
+  }
 
-function getAnimeEpisodes (id, start = 1, limit = 100) {
-  return animeAxios.get(`/anime/${id}/episodes?limit=${limit}&start=${start}`);
-}
-
-function getAnimeEpisode (id, episodeId) {
-  return animeAxios.get(`/anime/${id}/episode/${episodeId}`);
+  getAnimeEpisode (id, episodeId) {
+    return this.apiAxios.get(`/anime/${id}/episode/${episodeId}`);
+  }
 }
