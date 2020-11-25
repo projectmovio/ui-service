27f3df37256c113f073033eafa3fc2f5bb8761e1
fulder <misad90@gmail.com>
2020-11-25 23:55:22 +0100

    Disable flatpickr on not aired episodes

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -65,7 +65,7 @@ function createEpisodePage (animeEpisode, watchHistoryEpisode) {
               <div class="input-group-prepend">
                 <span class="input-group-text" id="inputGroup-sizing-sm" value="${latestWatchDate}">Watched</span>
               </div>
-              <input id="flatpickr" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+              <input id="flatpickr" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" ${!episodeAired ? 'disabled' : ''}>
             </div>
         </div>
     `;
