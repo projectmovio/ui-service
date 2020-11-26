b2de2731fc0a0cd04992259b7083e6a5f6080517
fulder <misad90@gmail.com>
2020-11-26 22:25:37 +0100

    Remove spaces between buttons

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -70,8 +70,7 @@ function createEpisodePage (animeEpisode, watchHistoryEpisode) {
 
               <input id="flatpickr" type="text" class="form-control" ${!episodeAired ? 'disabled' : ''}>
               <div class="input-group-append">
-                <button class="btn btn-primary" type="button" onclick="setCurrentWatchDate()"><i class="fas fa-calendar-day"></i></button>
-                <button class="btn btn-danger" type="button" onclick="removeWatchDate()"><i class="far fa-calendar-times"></i></button>
+                <button class="btn btn-primary" type="button" onclick="setCurrentWatchDate()"><i class="fas fa-calendar-day"></i></button><button class="btn btn-danger" type="button" onclick="removeWatchDate()"><i class="far fa-calendar-times"></i></button>
               </div>
             </div>
         </div>
