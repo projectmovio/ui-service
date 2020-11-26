ce5d0b497827f8fc8c3fb73835c7af4f90a858f7
fulder <misad90@gmail.com>
2020-11-26 22:50:33 +0100

    Enable bootstrap tooltips

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -90,6 +90,12 @@ function createEpisodePage (animeEpisode, watchHistoryEpisode) {
     weekNumbers: true,
     onClose: onCalendarClose,
   });
+
+  // Bootstrap enable tooltips
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 }
 
 function onCalendarClose (selectedDates, dateStr) {
